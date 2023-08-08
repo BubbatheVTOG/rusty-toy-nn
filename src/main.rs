17146c3e03a07b722b1b627e21510e90dca36c68
Bubba <bubbathevtog@gmail.com>
2023-08-07 21:45:30 -0400

    meh

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+mod math;
+use crate::math::MathOps;
+
+fn main() {
+    /*
+    let a = Matrix {
+        data: vec![vec![1.0, 2.0], vec![3.0, 4.0]],
+    };
+    let b = Matrix {
+        data: vec![vec![5.0, 6.0], vec![7.0, 8.0]],
+    };
+
+    let c = a.add(&b);
+    let d = a.sub(&b);
+    let e = a.mul(&b);
+
+    let f = Matrix {
+        data: vec![vec![0.0;5], vec![0.0;5]],
+    };
+
+
+
+    let h = f.randomize();
+
+    println!("Matrix - Random:\n{}", h);
+    println!("Matrix A + B:\n{}", c);
+    println!("Matrix A - B:\n{}", d);
+    println!("Matrix A * B:\n{}", e);
+
+    let identity = Matrix::identity(3);
+    println!("Identity Matrix:\n{}", identity);
+    */
+
+    let z = math::Matrix {
+        data: vec![vec![0.0;2], vec![0.0;2], vec![0.0;2]],
+    }.randomize();
+
+    let y = math::Matrix {
+        data: vec![vec![0.0;3], vec![0.0;3]],
+    }.randomize();
+
+    println!("Matrix Z:\n{}",z);
+    println!("Matrix Y:\n{}",y);
+    let res = z.dot_product(&y);
+    println!("Dot Product Z * Y:\n{}", res);
+
+    println!("Matrix Y:\n{}",y);
+    println!("Matrix Z:\n{}",z);
+    let res = y.dot_product(&z);
+    println!("Dot Product Y * Z:\n{}", res);
+}
---
+++ src/math.rs
@@ -0,0 +1,136 @@
+use rand::Rng;
+use std::fmt;
+
+pub trait MathOps<RHS, Output> {
+    fn add(&self, rhs: RHS) -> Output;
+    fn sub(&self, rhs: RHS) -> Output;
+    fn mul(&self, rhs: RHS) -> Output;
+    fn dot_product(&self, rhs: RHS) -> Output;
+}
+
+pub struct Matrix {
+    pub data: Vec<Vec<f64>>,
+}
+
+impl MathOps<&Matrix, Matrix> for Matrix {
+    fn sub(&self, rhs: &Matrix) -> Matrix {
+        assert_eq!(self.data.len(), rhs.data.len());
+        assert_eq!(self.data[0].len(), rhs.data[0].len());
+
+        let mut result = Matrix::new(self.data.len(), self.data[0].len());
+
+        for i in 0..self.data.len() {
+            for j in 0..self.data[i].len() {
+                result.data[i][j] = self.data[i][j] - rhs.data[i][j];
+            }
+        }
+
+        result
+    }
+
+    fn add(&self, rhs: &Matrix) -> Matrix {
+        assert_eq!(self.data.len(), rhs.data.len());
+        assert_eq!(self.data[0].len(), rhs.data[0].len());
+
+        let mut result = Matrix::new(self.data.len(), self.data[0].len());
+
+        for i in 0..self.data.len() {
+            for j in 0..self.data[i].len() {
+                result.data[i][j] = self.data[i][j] * rhs.data[i][j];
+            }
+        }
+
+        result
+    }
+
+    fn mul(&self, rhs: &Matrix) -> Matrix {
+        assert_eq!(self.data.len(), rhs.data.len());
+        assert_eq!(self.data[0].len(), rhs.data[0].len());
+
+        let mut result = Matrix::new(self.data.len(), self.data[0].len());
+
+        for i in 0..self.data.len() {
+            for j in 0..self.data[i].len() {
+                let mut sum = 0.0;
+                for k in 0..self.data[0].len() {
+                    sum += self.data[i][k] * rhs.data[k][j];
+                }
+                result.data[i][j] = sum;
+            }
+        }
+
+        result
+    }
+
+    fn dot_product(&self, rhs: &Matrix) -> Matrix {
+        assert_eq!(self.data.len(), rhs.data[0].len());
+
+        let mut result = Matrix::new(self.data.len(), rhs.data[0].len());
+
+        for row in 0..result.data.len() {
+            for col in 0..result.data[row].len() {
+                let mut sum = 0.0;
+                for k in 0..self.data[0].len() {
+                    sum += self.data[row][k] * rhs.data[k][col];
+                }
+                result.data[row][col] = sum;
+            }
+        }
+        result
+    }
+}
+
+impl Matrix {
+    fn new(rows: usize, cols: usize) -> Matrix {
+        Matrix {
+            data: vec![vec![0.0; cols]; rows],
+        }
+    }
+
+    pub fn from_vec(input:Vec<f32>) -> Matrix {
+        Matrix::new(input.len(),0)
+    }
+
+    pub fn randomize(&self) -> Matrix {
+        let mut rng = rand::thread_rng();
+        let mut result = Matrix::new(self.data.len(), self.data[0].len());
+        for i in 0..self.data.len() {
+            for j in 0..self.data[i].len() {
+                result.data[i][j] = rng.gen_range(0.0..1.0);
+            }
+        }
+        result
+    }
+
+
+    fn identity(size: usize) -> Matrix {
+        let mut matrix = Matrix::new(size, size);
+        for i in 0..size {
+            matrix.data[i][i] = 1.0;
+        }
+        matrix
+    }
+
+    pub fn transpose(&self) -> Matrix {
+        let mut result = Matrix::new(self.data[0].len(), self.data.len());
+        for i in 0..self.data.len() {
+            for j in 0..self.data[0].len() {
+                result.data[j][i] = self.data[i][j];
+            }
+        }
+        result
+    }
+}
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for row in &self.data {
+            for &element in row {
+                write!(f, "{:.2} ", element)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
